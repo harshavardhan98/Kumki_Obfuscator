56e664469187f3f94801aef2af10d004c39aa5ee
harshavardhanp <harshavardhan.zodiac@gmail.com>
2019-12-14 17:09:08 +0530

    Added fileOperations util class

---
+++ src/Utils/fileOperations.java
@@ -0,0 +1,58 @@
+package Utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+public class fileOperations {
+
+    public static void rename(String oldName,String newName){
+
+        //https://www.inf.unibz.it/~calvanese/teaching/06-07-ip/lecture-notes/uni09/node12.html
+
+        File f1 = new File("oldname.txt");
+        File f2 = new File("newname.txt");
+        boolean b = f1.renameTo(f2);
+
+    }
+
+    public static void copyFolder(File sourceFolder, File destinationFolder) throws IOException
+    {
+        // https://howtodoinjava.com/java/io/how-to-copy-directories-in-java/
+
+        //Check if sourceFolder is a directory or file
+        //If sourceFolder is file; then copy the file directly to new location
+        if (sourceFolder.isDirectory())
+        {
+            //Verify if destinationFolder is already present; If not then create it
+            if (!destinationFolder.exists())
+            {
+                destinationFolder.mkdir();
+                System.out.println("Directory created :: " + destinationFolder);
+            }
+
+            //Get all files from source directory
+            String files[] = sourceFolder.list();
+
+            //Iterate over all files and copy them to destinationFolder one by one
+            for (String file : files)
+            {
+                File srcFile = new File(sourceFolder, file);
+                File destFile = new File(destinationFolder, file);
+
+                //Recursive function call
+                copyFolder(srcFile, destFile);
+            }
+        }
+        else
+        {
+            //Copy the file content from one place to another
+            Files.copy(sourceFolder.toPath(), destinationFolder.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("File copied :: " + destinationFolder);
+        }
+    }
+
+
+
+}
