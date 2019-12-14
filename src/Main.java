4f0befb3b6add226080a7babc071271e8dba9140
Logesh Dinakaran <ddlogesh@gmail.com>
2019-12-14 12:32:12 +0530

    old commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,17 @@ import java.io.*;
 public class Main {
     public static void main(String[] args){
 
-
-
     }
 }
+
+// TODO: Rename variables
+/*
+S,5,$
+l,1,I
+0,o,Q
+_,_,_ (count of _)
+
+* Use same name if conflicts fails so that
+find & replace fails to work
+
+ */
