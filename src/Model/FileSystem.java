e259baa9d671d383e1f16eb1010927140b2c79c6
Logesh Dinakaran <ddlogesh@gmail.com>
2019-12-14 17:12:26 +0530

    static files integrated

---
+++ src/Model/FileSystem.java
@@ -0,0 +1,33 @@
+package Model;
+
+import java.util.List;
+
+public class FileSystem {
+    private String name;
+    private String type;
+    private List<FileSystem> files;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public List<FileSystem> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<FileSystem> files) {
+        this.files = files;
+    }
+}
