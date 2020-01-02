33282cd04e5f39aa5e5448c0fbe83ab4fa2f3f0c
harshavardhanp <harshavardhan.zodiac@gmail.com>
2020-01-02 19:56:50 +0530

    Comment Obfuscation added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import com.github.javaparser.JavaParser;
+import com.github.javaparser.Position;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.comments.Comment;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
@@ -9,6 +11,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import model.*;
 import utils.*;
@@ -25,9 +28,10 @@ public class Main {
         analyseProjectStructure();
         getDependencyData();
 
+        //removeComments();
         //MethodObfuscation();
         //ClassObfuscation();
-        PackageObfuscation();
+        //PackageObfuscation();
     }
 
     /***********************************************************/
@@ -100,6 +104,52 @@ public class Main {
     public static void MethodObfuscation() {
         MethodObfuscator mo = new MethodObfuscator();
         mo.obfuscate();
+    }
+
+    public static void removeComments(){
+
+        for(String s:classList){
+
+            try{
+                File f=new File(s);
+                Obfuscator obfuscator=new Obfuscator();
+                obfuscator.setCurrentFile(f);
+
+                CompilationUnit cu=JavaParser.parse(f);
+                cu.getComments();
+                List<Comment> l=cu.getComments();
+
+                for(Comment comment:l){
+
+                    if(comment.isLineComment()){
+                        ReplacementDataNode r=new ReplacementDataNode();
+                        Position begin=comment.getRange().orElse(null).begin;
+                        Position end=comment.getRange().orElse(null).end;
+                        r.setLineNo(begin.line);
+                        r.setStartColNo(begin.column);
+                        r.setEndColNo(end.column);
+                        r.setReplacementString("");
+                        obfuscator.setArrayList(r);
+                    }
+                    else if(comment.isBlockComment()){
+                        ReplacementDataNode r=new ReplacementDataNode();
+                        Position begin=comment.getRange().orElse(null).begin;
+                        Position end=comment.getRange().orElse(null).end;
+                        r.setLineNo(begin.line);
+                        r.setStartColNo(begin.column);
+                        r.setEndLineNo(end.line);
+                        r.setEndColNo(end.column);
+                        obfuscator.setArrayList(r);
+                    }
+                }
+
+                obfuscator.replaceComments();
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
     }
 
     /***********************************************************/
