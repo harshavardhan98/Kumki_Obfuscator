0a60faebe3f659a1e5e9fca6f954d64cf5f44988
harshavardhanp <harshavardhan.zodiac@gmail.com>
2020-01-23 14:36:09 +0530

    Completed handling iteration,conditional statements and constructors

--- src/utils/VariableObfuscation.java
+++ src/utils/VariableObfuscation.java
@@ -6,8 +6,7 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.expr.*;
-import com.github.javaparser.ast.stmt.BlockStmt;
-import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.stmt.*;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.ast.type.Type;
 import model.Obfuscator;
@@ -64,6 +63,22 @@ public class VariableObfuscation {
             Scope global_scope = new Scope();
             global_scope.setScope(null);
 
+            //Constructors
+            List<ConstructorDeclaration> constructors = clas.getConstructors();
+            if (!constructors.isEmpty()) {
+                for (ConstructorDeclaration constructor : constructors) {
+
+                    List<Parameter> parameterList = constructor.getParameters();
+                    if (!parameterList.isEmpty()) {
+                        for (Parameter p : parameterList)
+                            handleParameter(p,global_scope);
+                    }
+
+                    BlockStmt block = constructor.getBody();
+                    handleBlockStatement(block,global_scope);
+                }
+            }
+
             List<FieldDeclaration> global_fields = clas.getFields();
             if (!global_fields.isEmpty()) {
                 for (FieldDeclaration field : global_fields) {
@@ -94,8 +109,16 @@ public class VariableObfuscation {
             return;
 
         handleExpressionStatement(statement,parentScope);
-        //handleIfStatement(statement, parentScope);
         handleBlockStatement(statement, parentScope);
+        handleForStatement(statement,parentScope);
+        handleForEachStatement(statement,parentScope);
+        handleIfStatement(statement,parentScope);
+        handleReturnStatement(statement,parentScope);
+        handleSwitchStatement(statement,parentScope);
+        handleTryCatchStatement(statement,parentScope);
+        handleDoWhileStatement(statement,parentScope);
+        handleSynchronisedStatement(statement,parentScope);
+        handleExplicitConstructorInvocationStmt(statement,parentScope);
     }
 
     public void handleExpressionStatement(Statement st,Scope parentScope) {
@@ -275,6 +298,141 @@ public class VariableObfuscation {
             }
         }
     }
+
+    private void handleReturnStatement(Statement st,Scope parentScope) {
+        if (st == null || !st.isReturnStmt())
+            return;
+
+        handleExpression(st.asReturnStmt().getExpression().orElse(null),parentScope);
+    }
+
+    private void handleForStatement(Statement st,Scope parentScope) {
+
+        if (st == null || !st.isForStmt())
+            return;
+
+        Scope forLoopScope=new Scope();
+        forLoopScope.setScope(parentScope);
+
+        NodeList<Expression> n1 = st.asForStmt().getInitialization();
+        if (n1.isNonEmpty()) {
+            for (Expression e : n1)
+                handleExpression(e,forLoopScope);
+        }
+
+        handleExpression(st.asForStmt().getCompare().orElse(null),forLoopScope);
+
+        NodeList<Expression> n2 = st.asForStmt().getUpdate();
+        if (n2.isNonEmpty()) {
+            for (Expression e : n2)
+                handleExpression(e,forLoopScope);
+        }
+
+        Statement forStmt = st.asForStmt().getBody();
+        handleStatement(forStmt,forLoopScope);
+    }
+
+    private void handleForEachStatement(Statement st,Scope parentScope) {
+        if (st == null || !st.isForeachStmt())
+            return;
+
+        Scope forEachScope=new Scope();
+        forEachScope.setScope(parentScope);
+
+        NodeList<VariableDeclarator> variableDeclarators = st.asForeachStmt().getVariable().getVariables();
+        handleVariables(variableDeclarators,forEachScope);
+        handleExpression(st.asForeachStmt().getIterable(),forEachScope);
+        Statement forEach = st.asForeachStmt().getBody();
+        handleStatement(forEach,forEachScope);
+    }
+
+    public void handleIfStatement(Statement st, Scope parentScope) {
+        if (st == null || !st.isIfStmt())
+            return;
+
+        Scope ifScope;
+
+        Expression condition = st.asIfStmt().getCondition();
+        handleExpression(condition,parentScope);
+
+        Statement thenStmt = st.asIfStmt().getThenStmt();
+        ifScope = new Scope();
+        ifScope.setScope(parentScope);
+        handleStatement(thenStmt, ifScope);
+
+        Statement elseStmt = st.asIfStmt().getElseStmt().orElse(null);
+        ifScope = new Scope();
+        ifScope.setScope(parentScope);
+        handleStatement(elseStmt, ifScope);
+    }
+
+    public void handleSwitchStatement(Statement st,Scope parentScope) {
+        if (st == null || !st.isSwitchStmt())
+            return;
+
+        SwitchStmt switchStmt = st.asSwitchStmt();
+        handleExpression(switchStmt.getSelector(),parentScope);
+
+        List<SwitchEntryStmt> stList = switchStmt.getEntries();
+        if (stList != null) {
+            for (SwitchEntryStmt set : stList) {
+                handleExpression(set.getLabel().orElse(null),parentScope);
+                List<Statement> expst = set.getStatements();
+                if (!expst.isEmpty()) {
+                    for (Statement est : expst)
+                        handleStatement(est,parentScope);
+                }
+            }
+        }
+    }
+
+    private void handleTryCatchStatement(Statement st,Scope parentScope) {
+
+        if (st == null || !st.isTryStmt())
+            return;
+
+        handleStatement(st.asTryStmt().getTryBlock(),parentScope);
+
+        List<CatchClause> l = st.asTryStmt().getCatchClauses();
+        if (!l.isEmpty()) {
+            for (CatchClause i : l) {
+                handleParameter(i.getParameter(),parentScope);
+                handleStatement(i.getBody(),parentScope);
+            }
+        }
+    }
+
+    private void handleDoWhileStatement(Statement st,Scope parentScope){
+
+        if(st==null || !st.isDoStmt())
+            return;
+
+        handleStatement(st.asDoStmt().getBody(),parentScope);
+        handleExpression(st.asDoStmt().getCondition(),parentScope);
+    }
+
+    private void handleSynchronisedStatement(Statement st,Scope parentScope){
+
+        if(st==null || !st.isSynchronizedStmt())
+            return;
+
+        handleExpression(st.asSynchronizedStmt().getExpression(),parentScope);
+        handleStatement(st.asSynchronizedStmt().getBody(),parentScope);
+    }
+
+    public void handleExplicitConstructorInvocationStmt(Statement st,Scope parentScope) {
+        if (st == null || !st.isExplicitConstructorInvocationStmt())
+            return;
+
+        ExplicitConstructorInvocationStmt ecst = st.asExplicitConstructorInvocationStmt();
+        List<Expression> exp = ecst.getArguments();
+        if (exp != null) {
+            for (Expression e : exp)
+                handleExpression(e,parentScope);
+        }
+    }
+
+
 
 
     /*********************************************/
