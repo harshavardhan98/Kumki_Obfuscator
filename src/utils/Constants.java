20f447fad43c4b805ee59da5ab2b11b00d5de018
harshavardhanp <harshavardhan.zodiac@gmail.com>
2020-09-13 23:19:34 +0530

    Enabled constant obfuscation and changed the host directory

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -3,17 +3,13 @@ package utils;
 import java.io.File;
 
 public class Constants {
-    public static String projectDirectory = "/Users/harshavardhanp/final_year_project/bkp/ssn-app/";
-    //public static String projectDirectory = "/Users/logesh/Documents/SSN/kumkiTest/";
+    public static String projectDirectory = "/Users/harshavardhanp/AndroidStudioProjects/kumkiTestSSN";
     //public static String projectDirectory = "/Users/logesh/Documents/SSN/kumkiTestSSN/";
 
     public static String manifestPath = projectDirectory + "app" + File.separator + "src" + File.separator + "main" + File.separator + "AndroidManifest.xml";
     public static String projectRootDirectory = projectDirectory + "app" + File.separator + "src" + File.separator + "main" + File.separator + "java" + File.separator;
     public static String projectResDirectory = projectDirectory + "app" + File.separator + "src" + File.separator + "main" + File.separator + "res" + File.separator + "layout" + File.separator;
-
     public static String packageName = "in" + File.separator + "edu" + File.separator + "ssn" + File.separator + "ssnapp" + File.separator;
-    //public static String packageName = "com" + File.separator + "example" + File.separator + "dsc_onboarding" + File.separator;
-
     public static final String fileStructureJsonPath = "originalFileStructure.json";
 
     /**************************************************************************/
